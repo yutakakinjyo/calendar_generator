b4b107d956811ab70373d20b06ff200fec49c8dd
yutakakinjyo <yutakakinjyo@gmail.com>
2017-04-15 19:00:38 +0900

    add generate test

--- test/test_app.rb
+++ test/test_app.rb
@@ -11,9 +11,22 @@ class AppTest < Test::Unit::TestCase
     Sinatra::Application
   end
 
-  def test_it_fail_echo
+  def test_get_index_page
     get '/'
     assert last_response.ok?
+  end
+
+  def test_generate_func
+    param = Parameter.new
+    param.text = "sample event"
+    param.location =  "Japan"
+    param.details = "event detail"
+    param.start_date = "2017/10/10"
+    param.start_time = "100000"
+    param.end_date = "2017/10/10"
+    param.end_time = "110000"
+
+    assert_equal BASE_URL + "&text=sample event&location=Japan&details=event detail&dates=2017/10/10T100000/2017/10/10T110000", generate(param)
   end
 
 end
